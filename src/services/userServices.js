f3ce20f1fb0e8dc4adb6872f11699eafa696caeb
Daniel Porto <daporto@uninorte.edu.co>
2021-10-25 14:47:25 -0500

    feat(profile): profile is working

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,16 +1,25 @@
-import { post } from "./http";
+import { post, get } from "./http";
+
+const getUserByUsername = async (username, jwtToken) => {
+  const headers = {
+    "x-access-token": jwtToken
+  };
+  const allUsers = await get("/users", headers);
+  const userInfo = allUsers.data.find(user => user.username ===username)
+  return userInfo;
+}
 
 const loginUser = async (username, password) => {
   const user = { username, password };
   const response = await post("/users/login", user);
-  const json = response; 
+  const json = response;
   return json;
 };
 
 const signUpUser = async (name, username, email, password, passwordConfirmation) => {
-    const newUser = { name, username, email, password, passwordConfirmation };
-    const response = await post("/users", newUser);
-    const json = response; 
-    return json;
-  };
-export {loginUser, signUpUser};
+  const newUser = { name, username, email, password, passwordConfirmation };
+  const response = await post("/users", newUser);
+  const json = response;
+  return json;
+};
+export { loginUser, signUpUser, getUserByUsername};
